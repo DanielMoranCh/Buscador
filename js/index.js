f20b152da47a1f73afe23aef50a71e0e05903916
Daniel <dante_5210par@hotmail.com>
2018-04-07 02:42:04 -0500

    mostrar_todo
    carga de propiedad con boton mostrar todo

--- js/index.js
+++ js/index.js
@@ -1,3 +1,44 @@
+$(document).ready(function () {
+  let dropdown = $('#selectCiudad');
+
+  dropdown.empty();
+
+  dropdown.append('<option selected="true" disabled>Choose State/Province</option>');
+  dropdown.prop('selectedIndex', 0);
+
+  const url = 'data-1.json';
+
+  // Populate dropdown with list of provinces
+  $.getJSON(url, function (data) {
+    $.each(data, function (key, entry) {
+      dropdown.append($('<option></option>').attr('value', entry.Ciudad).text(entry.Ciudad));
+    })
+  });
+});
+
+
+function getDatos(){
+  //var HtmlNode = document.getElementById('datos');
+  var showData = $('#show-data');
+  $.getJSON('data-1.json', function (data) {
+
+    //  alert(data);
+
+      data.forEach(function(val , i){
+        //alert(val.Direccion);
+        var content = '<div class="tituloContenido card"><ul>'
+         + '<li> Direccion: ' + val.Direccion + '</li>'
+         + '<li> Ciudad: ' + val.Ciudad + '</li>'
+         + '<li> Telefono: ' + val.Telefono + '</li>'
+         +  '<li> Codigo_Postal: ' + val.Codigo_Postal + '</li>'
+         +  '<li> Tipo: ' + val.Tipo + '</li>'
+         +  '<li> Precio: ' + val.Precio + '</li>';
+        var list = $('<ul/></div>').html(content);
+        showData.append(list);
+      })
+    });
+}
+
 /*
   Creación de una función personalizada para jQuery que detecta cuando se detiene el scroll en la página
 */
