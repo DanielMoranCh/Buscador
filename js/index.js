7f79c328023325817cd81d530af2a4dd8d486129
Daniel <dante_5210par@hotmail.com>
2018-04-07 10:51:57 -0500

    iniciar_valores
    valores inicializados combo y range

--- js/index.js
+++ js/index.js
@@ -1,17 +1,46 @@
 $(document).ready(function () {
-  let dropdown = $('#selectCiudad');
-
-  dropdown.empty();
-
-  dropdown.append('<option selected="true" disabled>Choose State/Province</option>');
-  dropdown.prop('selectedIndex', 0);
+  let ciudad = $('#selectCiudad');
+  var datos = [];
+  var j = 0;
+  ciudad.empty();
+  ciudad.append('<option selected="true" disabled>Elige una ciudad</option>');
+  ciudad.prop('selectedIndex', 0);
 
   const url = 'data-1.json';
-
-  // Populate dropdown with list of provinces
+  // LISTA DE CIUDADES
   $.getJSON(url, function (data) {
     $.each(data, function (key, entry) {
-      dropdown.append($('<option></option>').attr('value', entry.Ciudad).text(entry.Ciudad));
+      var k = 0;
+      for(i = 0; i < datos.length; i++)
+         if(datos[i]==entry.Ciudad)
+           k++;
+      if(k==0)
+      {
+        datos[j]=entry.Ciudad;
+        j++;
+        ciudad.append($('<option></option>').attr('value', entry.Ciudad).text(entry.Ciudad));
+      }
+    })
+  });
+  let tipo = $('#selectTipo');
+  var datos = [];
+  var j = 0;
+  tipo.empty();
+  tipo.append('<option selected="true" disabled>Elige un tipo</option>');
+  tipo.prop('selectedIndex', 0);
+  // LISTA DE TIPOS
+  $.getJSON(url, function (data) {
+    $.each(data, function (key, entry) {
+      var k = 0;
+      for(i = 0; i < datos.length; i++)
+         if(datos[i]==entry.Tipo)
+           k++;
+      if(k==0)
+      {
+        datos[j]=entry.Tipo;
+        j++;
+        tipo.append($('<option></option>').attr('value', entry.Tipo).text(entry.Tipo));
+      }
     })
   });
 });
@@ -56,15 +85,53 @@ $.fn.scrollEnd = function(callback, timeout) {
 */
 
 function inicializarSlider(){
-  $("#rangoPrecio").ionRangeSlider({
-    type: "double",
-    grid: false,
-    min: 0,
-    max: 100000,
-    from: 200,
-    to: 80000,
-    prefix: "$"
-  });
+    const url = 'data-1.json';
+    var minimo = 100000;
+    var maximo= 0;
+    var mitad =0;
+    var datos = [];
+    var j = 0;
+    $.getJSON(url, function (data) {
+      $.each(data, function (key, entry) {
+        let precio = entry.Precio.replace(",", "");
+        precio =  precio.replace("$", "");
+        //alert(parseFloat(precio));
+        var k = 0;
+        for(i = 0; i < datos.length; i++)
+           if(datos[i]==parseFloat(precio))
+             k++;
+        if(k==0)
+        {
+
+        //  alert(parseFloat(precio)+10000000);
+          datos[j]=parseFloat(precio);
+            //alert(datos[j]);
+          j++;
+        }
+      })
+
+      for(i = 0; i < datos.length; i++)
+      {
+        //alert(datos.length);
+        if(datos[i]<minimo)
+          minimo=datos[i];
+        if(datos[i]>maximo)
+          maximo=datos[i];
+      }
+
+      $("#rangoPrecio").ionRangeSlider({
+        type: "double",
+        grid: false,
+        min: 0,
+        max: 100000,
+        from: minimo,
+        to: maximo,
+        prefix: "$"
+      });
+
+    });
+    //alert(datos[4]);
+
 }
 /*
   Función que reproduce el video de fondo al hacer scroll, y deteiene la reproducción al detener el scroll
